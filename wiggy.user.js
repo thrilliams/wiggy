49e611b7bf732a569886ebec598b1e1bdcb817f3
june <juniper@aethre.co>
2024-04-05 10:10:31 -0400

    replace that boring raw dom manipulation with some hot new preact

--- wiggy.user.js
+++ wiggy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         wiggy
 // @namespace    https://aethre.co/
-// @version      2024-04-04
+// @version      2024-04-05
 // @description  makes whenisgood slightly more bearable
 // @author       june
 // @match        https://whenisgood.net/*/results/*
@@ -9,64 +9,181 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
+/* boilerplate */
 "use strict";
 
-const paint = () => {
-	unsafeWindow.paintCanDos();
-	unsafeWindow.paintRespondents();
+// this removes the default WiG systems
+const oldContainer = document.querySelector(".respondents");
+for (const node of document.querySelectorAll(
+	".respondents > :not(.respondentPopup)"
+))
+	node.remove();
 
-	for (const respondentKey in unsafeWindow.respondents) {
-		const respondent = unsafeWindow.respondents[respondentKey];
-		const respondentToggle = unsafeWindow.document
-			.getElementById(respondent.id)
-			.querySelector("input");
-		if (respondentToggle !== null)
-			respondentToggle.checked = respondent.included;
-	}
+const appContainer = document.createElement("div");
+appContainer.style.marginLeft = "10px";
+oldContainer.after(appContainer);
+
+// sometimes imports take a sec
+appContainer.innerText = "loading...";
+
+// this tweaks table formatting slightly to make it consistent and avoid layout shifting
+for (const element of document.querySelectorAll("td.slot"))
+	element.style.minWidth = "90px";
+
+// hack to make name highlighting (specifically, de-highlighting) work again
+const outTdOriginal = unsafeWindow.outTd;
+unsafeWindow.outTd = () => {
+	outTdOriginal();
+	for (const key of Object.keys(unsafeWindow.respondents))
+		document.getElementById(key.slice(1)).className = "";
 };
 
-/* adds a button to toggle each response */
-const toggleAllButton = document.createElement("button");
-toggleAllButton.innerText = "toggle all";
-toggleAllButton.style.margin = "0 6px 6px 10px";
+unsafeWindow.paintRespondents = () => {};
 
-toggleAllButton.addEventListener("click", () => {
-	for (const respondentKey in unsafeWindow.respondents) {
-		const respondent = unsafeWindow.respondents[respondentKey];
-		respondent.included = !respondent.included;
+// imports from esm.sh, using dynamic import syntax
+const { render, h, Fragment } = await import(
+	"https://esm.sh/preact@10.20.1?exports=render,h,Fragment"
+);
+const { useState } = await import(
+	"https://esm.sh/preact@10.20.1/hooks?exports=useState"
+);
+const { default: htm } = await import(
+	"https://esm.sh/htm@3.1.1?exports=default"
+);
+
+const { default: Fuse } = await import(
+	"https://cdn.jsdelivr.net/npm/fuse.js@7.0.0/dist/fuse.min.mjs"
+);
+
+// helper to use jsx-adjacent syntax without bundling
+const html = htm.bind(h);
+
+/* the app */
+const respondents = new Map(
+	Object.keys(unsafeWindow.respondents).map((id) => [
+		id.slice(1),
+		unsafeWindow.respondents[id],
+	])
+);
+
+const respondentIds = [...respondents.keys()];
+
+const fuse = new Fuse(
+	Object.keys(unsafeWindow.respondents).map(
+		(id) => unsafeWindow.respondents[id]
+	),
+	{
+		keys: ["name"],
+		threshold: 0.2,
+		includeScore: true,
 	}
+);
 
-	paint();
-});
+function Respondent({ id, included, setIncluded }) {
+	const respondent = respondents.get(id);
 
-unsafeWindow.document
-	.querySelector(
-		"body > table:nth-child(9) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > h4:nth-child(3)"
-	)
-	.after(toggleAllButton);
-
-/* adds a toggle to each respondent to toggle them quickly */
-for (const respondentKey in unsafeWindow.respondents) {
-	const respondent = unsafeWindow.respondents[respondentKey];
-
-	const respondentToggle = document.createElement("input");
-	respondentToggle.type = "checkbox";
-	respondentToggle.checked = respondent.included;
-
-	respondentToggle.addEventListener("change", (event) => {
-		respondent.included = respondentToggle.checked;
-		paint();
-	});
-
-	// stops the response popup from triggering on toggle
-	respondentToggle.addEventListener("click", (event) => {
-		// event.stopImmediatePropagation();
-		event.stopPropagation();
-	});
-
-	const respondentDiv = unsafeWindow.document.getElementById(respondent.id);
-	respondentDiv.style.display = "flex";
-	respondentDiv.style["justify-content"] = "space-between";
-	respondentDiv.style["align-items"] = "center";
-	respondentDiv.append(respondentToggle);
+	return html`<div
+		style=${{
+			display: "flex",
+			justifyContent: "space-between",
+			alignItems: "center",
+		}}
+	>
+		<span
+			id=${id}
+			style=${{ cursor: "pointer" }}
+			onClick=${() => {
+				const dummyDiv = document.createElement("div");
+				dummyDiv.id = id;
+				unsafeWindow.clickRespondent(dummyDiv);
+			}}
+			>${respondent?.name}</span
+		>
+		<input
+			type="checkbox"
+			checked=${included}
+			onChange=${(event) => setIncluded(event.target.checked)}
+		/>
+	</div>`;
 }
+
+function App() {
+	const [includedIds, setIncludedIdsRaw] = useState(respondentIds);
+
+	const setIncludedIds = (newIds) => {
+		for (const [id, respondent] of respondents.entries())
+			respondent.included = newIds.includes(id);
+		setIncludedIdsRaw(newIds);
+		unsafeWindow.paintCanDos();
+	};
+
+	const setIncluded = (id, value) => {
+		if (value) {
+			setIncludedIds([...includedIds, id]);
+		} else {
+			setIncludedIds(
+				includedIds.filter((includedId) => includedId !== id)
+			);
+		}
+	};
+
+	const [searchQuery, setSearchQuery] = useState("");
+	const [sortMode, setSortMode] = useState("fuse");
+
+	const shownRespondents =
+		searchQuery.length > 0
+			? fuse.search(searchQuery).map((result) => result.item)
+			: [...respondents.values()];
+	if (sortMode === "name")
+		shownRespondents.sort((a, b) => a.name.localeCompare(b.name));
+	if (sortMode === "date")
+		shownRespondents.sort(
+			(a, b) =>
+				new Date(a.updated).getTime() - new Date(b.updated).getTime()
+		);
+
+	return html`<div
+		style=${{ display: "flex", flexDirection: "column", gap: "5px" }}
+	>
+		<button
+			onClick=${() =>
+				setIncludedIds(
+					respondentIds.filter((id) => !includedIds.includes(id))
+				)}
+		>
+			toggle all
+		</button>
+		<select onChange=${(event) => setSortMode(event.target.value)}>
+			<option value="name" selected=${sortMode === "name"}>
+				sort by name
+			</option>
+			<option value="date" selected=${sortMode === "date"}>
+				sort by date
+			</option>
+			<option value="fuse" selected=${sortMode === "fuse"}>
+				sort by relevance
+			</option>
+		</select>
+		<input
+			type="text"
+			placeholder="search..."
+			value=${searchQuery}
+			onInput=${(event) => setSearchQuery(event.target.value)}
+		/>
+		<hr style=${{ width: "100%", margin: "0" }} />
+		${shownRespondents.map(
+			({ id }) =>
+				html`<${Respondent}
+					id=${id}
+					key=${id}
+					included=${includedIds.includes(id)}
+					setIncluded=${(value) => setIncluded(id, value)}
+				/>`
+		)}
+	</div>`;
+}
+
+/* rendering */
+
+appContainer.innerText = "";
+render(html`<${App} />`, appContainer);
